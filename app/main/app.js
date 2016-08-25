54084562be7e600a9e3a3a4e0c0c81ca47a11e6a
haseeb bhatti <haseebhatti@gmail.com>
2016-08-25 08:40:16 -0400

    uploaded setup and dropdown button

--- app/main/app.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  'use strict';
-  function MyController () {
-    this.name = 'This is my controller';
-  }
-  angular.module('myApp', [])
-    .controller('MyController', MyController);
-}());
